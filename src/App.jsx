758e10cb34783c6f83b82e08e9362aa56778128a
ChengXu Pan <chengxupan@ChengXudeMacBook-Pro.local>
2024-10-28 00:44:29 +0800

    Build an embed component

--- src/App.jsx
+++ src/App.jsx
@@ -11,3 +11,141 @@ function App() {
 }
 
 export default App
+ 
+// 了解了，你想要製作一個可以像外部庫一樣直接導入的可重用元件。這樣的元件通常可以打包成 npm 套件，讓其他項目能夠輕鬆安裝和使用。以下是實現這個目標的步驟：
+
+// 1. 建立元件
+// 首先，創建你的滿意度問卷元件，這個元件的結構可以參考前面的範例。將它放在一個單獨的文件中，例如 SatisfactionSurvey.js。
+
+// jsx
+// Copy code
+// // SatisfactionSurvey.js
+// import React, { useState } from 'react';
+// import Modal from 'react-modal';
+
+// const SatisfactionSurvey = ({ isOpen, onRequestClose }) => {
+//     return (
+//         <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+//             <h2>滿意度問卷</h2>
+//             <form>
+//                 <label>
+//                     您的滿意度評價：
+//                     <select>
+//                         <option value="1">1</option>
+//                         <option value="2">2</option>
+//                         <option value="3">3</option>
+//                         <option value="4">4</option>
+//                         <option value="5">5</option>
+//                     </select>
+//                 </label>
+//                 <button type="submit">提交</button>
+//             </form>
+//             <button onClick={onRequestClose}>關閉</button>
+//         </Modal>
+//     );
+// };
+
+// export default SatisfactionSurvey;
+// 2. 設置專案結構
+// 在你的專案中，確保有一個清晰的結構，例如：
+
+// css
+// Copy code
+// my-satisfaction-survey/
+// ├── src/
+// │   ├── SatisfactionSurvey.js
+// │   └── index.js
+// ├── package.json
+// └── README.md
+// 3. 編寫 index.js
+// 在 src/index.js 中導出你的元件，這樣使用者可以輕鬆導入。
+
+// javascript
+// Copy code
+// // src/index.js
+// import SatisfactionSurvey from './SatisfactionSurvey';
+
+// export default SatisfactionSurvey;
+// 4. 建立 package.json
+// 在專案根目錄中創建 package.json，設置基本信息。可以用 npm init 命令生成，然後根據需要修改。
+
+// json
+// Copy code
+// {
+//   "name": "my-satisfaction-survey",
+//   "version": "1.0.0",
+//   "main": "src/index.js",
+//   "license": "MIT",
+//   "dependencies": {
+//     "react": "^17.0.0",
+//     "react-dom": "^17.0.0",
+//     "react-modal": "^3.14.3"
+//   }
+// }
+// 5. 編譯和打包
+// 你可以使用工具（如 Babel 或 Webpack）來編譯和打包你的元件。這樣可以生成一個可供其他項目使用的版本。這裡是使用 Babel 的基本範例：
+
+// 安裝 Babel 及其 presets：
+// bash
+// Copy code
+// npm install --save-dev @babel/core @babel/preset-env @babel/preset-react babel-loader webpack webpack-cli
+// 創建一個 webpack.config.js：
+// javascript
+// Copy code
+// const path = require('path');
+
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//         path: path.resolve(__dirname, 'dist'),
+//         filename: 'index.js',
+//         libraryTarget: 'commonjs2',
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.jsx?$/,
+//                 exclude: /node_modules/,
+//                 use: {
+//                     loader: 'babel-loader',
+//                     options: {
+//                         presets: ['@babel/preset-env', '@babel/preset-react']
+//                     }
+//                 }
+//             }
+//         ]
+//     },
+//     externals: {
+//         react: 'commonjs react',
+//         'react-dom': 'commonjs react-dom',
+//     }
+// };
+// 6. 發布到 npm
+// 在發佈之前，你需要先登入 npm：
+
+// bash
+// Copy code
+// npm login
+// 然後發佈你的包：
+
+// bash
+// Copy code
+// npm publish
+// 7. 在其他項目中使用
+// 一旦你的元件發佈到 npm，其他開發者就可以使用以下命令安裝：
+
+// bash
+// Copy code
+// npm install my-satisfaction-survey
+// 然後在他們的 React 應用中導入並使用你的元件：
+
+// jsx
+// Copy code
+// import SatisfactionSurvey from 'my-satisfaction-survey';
+
+// // 使用元件
+// <SatisfactionSurvey isOpen={true} onRequestClose={() => {}} />
+// 這樣就完成了從外部引入可重用的滿意度問卷元件的過程。如果你需要進一步的幫助，隨時告訴我！
+
+
+
